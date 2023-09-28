f900a1746510ce895921102811eeac0fb231248c
Graham Spratt <37407811+grahamspr86@users.noreply.github.com>
2023-09-28 18:06:58 +0100

    Added DMP Play Mode Option

--- actions.js
+++ actions.js
@@ -18,6 +18,16 @@ module.exports = function (self) {
 					label: 'Zone ID (leave blank to use default set in connection settings)',
 					id: 'sign_id',
 				},
+				{
+					type: 'dropdown',
+					label: 'Play Mode',
+					id: 'play_mode',
+					default: '0',
+					choices: [
+					  { id: '0', label: 'OneTime' },
+					  { id: '1', label: 'Continuous' }
+					],
+				},
 			],
 			callback: async (event) => {
 				console.log('sending play_file command')
